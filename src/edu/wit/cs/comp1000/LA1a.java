e378463bc20de5747a0547ca13f8521b9ee29ab6
sam19650 <116615725+sam19650@users.noreply.github.com>
2022-10-25 08:22:18 -0400

    Update LA1a.java

--- src/edu/wit/cs/comp1000/LA1a.java
+++ src/edu/wit/cs/comp1000/LA1a.java
@@ -18,31 +18,6 @@ public class LA1a {
 	 */
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.printf("Enter five numbers: ");
-		final double a = scanner.nextDouble();
-		final double b = scanner.nextDouble();
-		final double c = scanner.nextDouble();
-		final double d = scanner.nextDouble();
-		final double e = scanner.nextDouble();
-		
-		scanner.close();
-		
-		final double sum = (a + b + c + d + e);
-		final double mean = (sum / 5.0);
-		
-		final double dA = (a - mean);
-		final double dB = (b - mean);
-		final double dC = (c - mean);
-		final double dD = (d - mean);
-		final double dE = (e - mean);
-		
-		final double stdDev = Math.sqrt((dA*dA + dB*dB + dC*dC + dD*dD + dE*dE) / 5.0);
-		
-		System.out.printf("Sum: %.2f%n", sum);
-		System.out.printf("Mean: %.2f%n", mean);
-		System.out.printf("Population Standard Deviation: %.2f%n", stdDev);
 	}
 
 }
